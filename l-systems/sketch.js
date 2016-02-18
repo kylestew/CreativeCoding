88a8c1e2d4996f8da382dac2eb79d1189ec1c82e
Kyle Stewart <kylestew@gmail.com>
2016-02-18 01:40:58 -0800

    Basic animated l system demo in js

--- l-systems/sketch.js
+++ l-systems/sketch.js
@@ -18,79 +18,126 @@
 //   "f" : "ffffff"
 // };
 
-// ???
-var axiom = "F-F-F-F";
-var rules = {
-  "F" : "FF-F-F-F-FF",
-};
-
-var step = 64;
-var angleInc = 90;
-
-var lString;
+var lSystem;
+var step, skip;
 
 function setup() {
-  createCanvas(640, 640);
-  frameRate(1);
+  createCanvas(600, 600);
+  frameRate(30);
   stroke(255);
   noFill();
 
-  lString = axiom;
-}
+  // ???
+  // lSystem = new LSystem({
+  //   axiom: "F",
+  //   rules: {
+  //     "F": "+F--F+"
+  //   },
+  //   theta: radians(45)
+  // })
+  // lSystem.iterate(12);
 
-var iterateLString = function(lString, rules) {
-  return _.map(lString.split(""), function(symbol) {
-    var replacement = _.find(rules, function(value, key) {
-      return symbol == key;
-    });
-    return replacement || symbol;
-  }).join("");
-}
+  // ???
+  // lSystem = new LSystem({
+  //   axiom: "A",
+  //   rules: {
+  //     "B": "A+B+A",
+  //     "A": "B-A-B"
+  //   },
+  //   theta: radians(60)
+  // })
+  // lSystem.iterate(8);
 
-var parseLStringToVertices = function(lString, step, angleInc) {
-  var pos = createVector(0, 0);
-  var angle = 0;
+  // ???
+  lSystem = new LSystem({
+    axiom: "F",
+    rules: {
+      "A": "-F+AA++A+F--F-A",
+      "F": "F+A++A-F--FF-A+"
+    },
+    theta: radians(60)
+  })
+  lSystem.iterate(5);
 
-  var vertices = [];
-  vertices.push([pos.x, pos.y]);
-  _.each(lString.split(""), function(command) {
-    switch(command) {
-      case 'F':
-        pos.x += step*cos(radians(angle));
-        pos.y += step*sin(radians(angle));
-        vertices.push([pos.x, pos.y]);
-        break;
-      case 'f':
-        pos.x += step*cos(radians(angle));
-        pos.y += step*sin(radians(angle));
-        break;
-      case '+':
-        angle += 90;
-        break;
-      case '-':
-        angle -= 90;
-        break;
-    }
-  });
+  // Pentigree
+  // lSystem = new LSystem({
+  //   axiom: "F-F-F-F",
+  //   rules: {
+  //     "F": "F-F+F+FF-F-F+F"
+  //   },
+  //   theta: radians(90)
+  // })
+  // lSystem.iterate(3);
 
-  return vertices;
+  // try to center
+  background(1);
+  translate(width/2, height/3);
+  step = 3;
+  skip = 256;
+  $('#generation-text').text("generation: " + lSystem.generation);
 }
 
 function draw() {
-  background(0);
-
-  lString = iterateLString(lString, rules);
-  var vertices = parseLStringToVertices(lString, step/(9*(frameCount)), angleInc);
-
-  // TODO: auto center based on vertices created
-  translate(200, 450);
-
-  beginShape();
-  _.each(vertices, function(v) {
-    vertex(v[0], v[1]);
-  });
-  endShape(CLOSE);
-
-  if (frameCount > 5)
+  if (lSystem.renderComplete)
     noLoop();
+  lSystem.renderStep(skip, step);
 }
+
+var LSystem = function LSystem(props) {
+  this.axiom = props.axiom;
+  this.rules = props.rules;
+  this.theta = props.theta;
+
+  this.production = this.axiom;
+  this.generation = 0;
+
+  this.turtle = createVector(0, 0);
+  this.angle = 0;
+  this.renderComplete = false;
+}
+LSystem.prototype.iterate = function(iterations) {
+  iterations = iterations || 1;
+  // console.time("iterate");
+
+  for (var itr = 0; itr < iterations; itr++) {
+    var curProd = this.production;
+    var nextProd = "";
+    for (var i = 0; i < curProd.length; i++) {
+      var symbol = curProd.charAt(i);
+      var replacement = this.rules[symbol];
+      nextProd += replacement || symbol;
+    }
+    this.production = nextProd;
+    this.generation++;
+  }
+
+  // console.log("production length: " + this.production.length);
+  // console.timeEnd("iterate");
+};
+LSystem.prototype.renderStep = function(steps, length) {
+  for (var i = 0; i < steps; i++) {
+    switch(this.production.charAt(i)) {
+      case 'f': // skip
+        this.turtle.x += length*cos(this.angle);
+        this.turtle.y += length*sin(this.angle);
+        break;
+      case '+':
+        this.angle += this.theta;
+        break;
+      case '-':
+        this.angle -= this.theta;
+        break;
+      default:
+        var x = this.turtle.x + length*cos(this.angle);
+        var y = this.turtle.y + length*sin(this.angle);
+        line(this.turtle.x, this.turtle.y, x, y);
+        this.turtle.x = x;
+        this.turtle.y = y;
+        break;
+    }
+  }
+
+  this.production = this.production.substring(steps);
+  if (this.production == "")
+    this.renderComplete = true;
+};
