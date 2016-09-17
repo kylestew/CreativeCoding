6cb8a3f6a26421469f8ed71b2590e4784579986c
Kyle Stewart <kylestew@gmail.com>
2016-09-17 12:05:24 -0700

    Can read BATT VCC

--- SocialMotes/mote/mote.ino
+++ SocialMotes/mote/mote.ino
@@ -10,11 +10,12 @@
 const int STATUS_LED = 5;
 
 /* Wifi */
-const char* ssid = "subterranean";
-const char* password = "subterranean42";
+const char* ssid = "TRUMP 4 PREZ";
+const char* password = "monkeybusine$$";
 
 /* UDP */
-byte UDP_REMOTE_IP[] = { 10, 0, 0, 52 };
+// nNeed to assign static IP to master control
+byte UDP_REMOTE_IP[] = { 192, 168, 0, 11 };
 int UDP_PORT = 2390;
 WiFiUDP udpConn;
 
@@ -22,6 +23,11 @@ WiFiUDP udpConn;
 const int NEO_PIXEL_PIN = 4; // D4 is pixel pin
 const int NEO_PIXEL_COUNT = 1;
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NEO_PIXEL_COUNT, NEO_PIXEL_PIN, NEO_GRB + NEO_KHZ400);
+
+/* Checking */
+const int LOOP_SLEEP = 20;
+int checkInDelay = 0;
+const int checkInReset = 250; // 250 * 20 = 5 seconds
 
 
 void setup() {
@@ -32,7 +38,115 @@ void setup() {
   digitalWrite(STATUS_LED, LOW);
 
   // boot up wifi
-  byte ledStatus = LOW;
+  beginNetwork();
+
+  // init IMU
+  beginIMU();
+  
+  // initialze NeoPixels
+  pixels.begin();
+  pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+  pixels.show();
+}
+
+void loop() {
+  // incoming
+  OSCBundle msgIn;
+  int size;
+  if( (size = udpConn.parsePacket()) > 0) {
+    while(size--)
+      msgIn.fill(udpConn.read());
+
+    if (!msgIn.hasError()) {      
+      msgIn.route("/led", routeLed);
+    } else {
+      Serial.println("OSC MESSAGE ERROR");
+      Serial.println(msgIn.getError());
+    }
+  }
+  
+
+  delay(LOOP_SLEEP); // allow ESP8266 to do some work in the background
+  // TODO: don't use loop - switch to interrupts
+  // can we interrupt on incoming OSC?
+
+
+  // check in with master control ever so often
+  if (checkInDelay++ == 0)
+    sayHi();
+  if (checkInDelay > checkInReset)
+    checkInDelay = 0;
+}
+
+
+// === OSC ===
+void sendOSCMessage(OSCMessage msg) {
+  udpConn.beginPacket(UDP_REMOTE_IP, UDP_PORT);
+  msg.send(udpConn); 
+  udpConn.endPacket();
+
+  msg.empty(); 
+}
+
+void sayHi() {
+  // check in with master control
+  char myIpString[24];
+  IPAddress myIp = WiFi.localIP();
+  sprintf(myIpString, "%d.%d.%d.%d", myIp[0], myIp[1], myIp[2], myIp[3]);
+
+  OSCMessage msg("/hi");
+  msg.add(myIpString);
+
+  // read ADC for current batt voltage
+  int battVcc = analogRead(A0);
+  Serial.println("BATT VCC:");
+  Serial.println(battVcc);
+  msg.add((int32_t)battVcc);
+
+  sendOSCMessage(&msg);
+}
+
+//converts the pin to an osc address
+char * numToOSCAddress( int pin){
+    static char s[10];
+    int i = 9;
+  
+    s[i--]= '\0';
+  do
+    {
+    s[i] = "0123456789"[pin % 10];
+                --i;
+                pin /= 10;
+    }
+    while(pin && i);
+    s[i] = '/';
+    return &s[i];
+}
+
+
+// === ROUTES ===
+/*
+ * /led/0 R(int) G(int) B(int)
+ */
+void routeLed(OSCMessage &msg, int addrOffset) {
+//  for(byte led = 0; led < NEO_PIXEL_COUNT; led++) {
+//    int ledMatched = msg.match(numToOSCAddress(led), addrOffset);
+//    if(ledMatched) {
+
+      int r = msg.getInt(0);
+      int g = msg.getInt(1);
+      int b = msg.getInt(2);
+      
+      pixels.setPixelColor(0, pixels.Color(r, g, b));
+      pixels.show();
+//    }
+//  }
+}
+
+
+// === NETWORK ===
+void beginNetwork() {
+    byte ledStatus = LOW;
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.mode(WIFI_STA); // connect to local wifi, don't make an access point
@@ -52,102 +166,10 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-
-  // init IMU
-  //...
-  
-  // initialze NeoPixels
-  pixels.begin();
-  pixels.setPixelColor(0, pixels.Color(0, 0, 0));
-  pixels.show();
-
-  // register with master control
-  char myIpString[24];
-  IPAddress myIp = WiFi.localIP();
-  sprintf(myIpString, "%d.%d.%d.%d", myIp[0], myIp[1], myIp[2], myIp[3]);
-  sendOSCMessage("/hi", myIpString);
-}
-
-void sendOSCMessage(const char * message, int value) {
-  OSCMessage msg(message);
-  msg.add((int32_t)value);
-  
-  udpConn.beginPacket(UDP_REMOTE_IP, UDP_PORT);
-  msg.send(udpConn); 
-  udpConn.endPacket();
-
-  msg.empty(); 
-}
-
-void sendOSCMessage(const char * message, const char * value) {
-  OSCMessage msg(message);
-  msg.add(value);
-  
-  udpConn.beginPacket(UDP_REMOTE_IP, UDP_PORT);
-  msg.send(udpConn); 
-  udpConn.endPacket();
-
-  msg.empty(); 
-}
-
-/*
- * /led/0 R(int) G(int) B(int)
- */
-void routeLed(OSCMessage &msg, int addrOffset) {
-//  for(byte led = 0; led < NEO_PIXEL_COUNT; led++) {
-//    int ledMatched = msg.match(numToOSCAddress(led), addrOffset);
-//    if(ledMatched) {
-
-      int r = msg.getInt(0);
-      int g = msg.getInt(1);
-      int b = msg.getInt(2);
-      
-      pixels.setPixelColor(0, pixels.Color(r, g, b));
-      pixels.show();
-//    }
-//  }
-}
-
-void loop() {
-  // incoming
-  OSCBundle msgIn;
-  int size;
-  if( (size = udpConn.parsePacket()) > 0) {
-    while(size--)
-      msgIn.fill(udpConn.read());
-
-    if (!msgIn.hasError()) {      
-      msgIn.route("/led", routeLed);
-    } else {
-      Serial.println("OSC MESSAGE ERROR");
-      Serial.println(msgIn.getError());
-    }
-  }
-
-
-// TODO: don't send forever
-  
-  delay(20);
 }
 
 
-
-
-
-//converts the pin to an osc address
-char * numToOSCAddress( int pin){
-    static char s[10];
-    int i = 9;
-  
-    s[i--]= '\0';
-  do
-    {
-    s[i] = "0123456789"[pin % 10];
-                --i;
-                pin /= 10;
-    }
-    while(pin && i);
-    s[i] = '/';
-    return &s[i];
+// === IMU ===
+void beginIMU() {
 }
 
---
+++ SocialMotes/readme.md
@@ -0,0 +1,64 @@
+== NOTES ==
+OSC Lib
+https://github.com/sandeepmistry/esp8266-OSC
+
+
+
+== TODOs ==
+
++ Get IMU online
++ Order parts
+
++ Heart beat mode
++ Color on rotate mode
++ Shake fireflies
+
++ Control modes via MaxMSP
+
+
+
++ Positional tracking in room?
++ How long will the battery last
++ Get proper resistor/cap setup for LEDs
++ Enable low power sleep mode - cross wiring
+
+
+
+== BOM ==
+
+ESP8266 Feather Huzzah
+https://www.adafruit.com/products/2821
+$15.95
+
+Sparkfun 9DOF
+https://www.sparkfun.com/products/13944
+$14.95
+(cheaper versions available)
+
+Li-Poly battery
+https://www.adafruit.com/products/258
+$9.95
+
+LEDs
+https://www.adafruit.com/products/1312
+https://www.adafruit.com/products/1612
+$4.95
+
+Perfboard or Breadboards
+https://www.sparkfun.com/products/13268
+https://www.adafruit.com/products/64
+~$2
+
+Proper resistors for Battery reading
+
+
+
+
+
+
+
+
+== Later ==
+Extender cable for charge/program port
+wired reset button
+Touch sensitive paint
